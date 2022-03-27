f44adb2a206b54318ea16b8fd458f29218b89b12
Faizan Alam <faizanalam2030@gmail.com>
2022-03-27 20:45:06 +0530

    final

--- call_config.py
+++ call_config.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 import speak
 def call() :
     client = Client("ACc7d414df5a2772ddffae6e54f79f6756",
-                "57da55e22e5fa854d28076cea8024872")
+                "b440d2279745329a26aaf3b42d0122d2")
     call= client.calls.create(twiml='<Response><Say>Hello</Say></Response>',to = "+918210410103",  from_ = "+13512072081" )
     print("done")  
     speak.speak("Successfully Called to Doctor Sir!")  
--- sms_config.py
+++ sms_config.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 import speak
 
 def sms(text) :
-    client = Client("ACc7d414df5a2772ddffae6e54f79f6756" , "57da55e22e5fa854d28076cea8024872")
+    client = Client("ACc7d414df5a2772ddffae6e54f79f6756" , "b440d2279745329a26aaf3b42d0122d2")
     client.messages.create(to = ["+918210410103"],
        from_ = "+13512072081",
        body = text)
